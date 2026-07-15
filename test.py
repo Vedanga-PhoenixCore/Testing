8a0b246debd313aa2e4e2bcb72b6d51e0e0c2759
Vedanga Bendre <vedanga@phoenixcoresystems.com>
2026-07-15 15:41:12 +0530

    Updating test.py to check Webhook working

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ def calculate_area(length, width):
     This is a minimal test function to verify the cloud pipeline 
     is successfully catching webhooks and parsing logic.
     Adding Bit More content to this function to ensure that the webhook is triggered.
+    Altering to check the git pull works for webhook
     """
     area = length * width
     return area
